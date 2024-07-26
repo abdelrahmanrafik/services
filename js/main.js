177b616532fbf599509f1733ded8b488e1a41f20
abdelrahmanrafik <111228517+abdelrahmanrafik@users.noreply.github.com>
2024-07-26 18:12:00 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(function(){
+    $(".grid").masonry({ itemSelector: ".grid-item" });
+    
+    $(".filtering").on("click", "span", function () {
+        var a = $(".gallery").isotope({});
+        var e = $(this).attr("data-filter");
+        a.isotope({ filter: e });
+    });
+    $(".filtering").on("click", "span", function () {
+        $(this).addClass("active").siblings().removeClass("active");
+    });
+}) 
+
+
+const counters = document.querySelectorAll('.value');
+const speed = 200;
+
+counters.forEach( counter => {
+   const animate = () => {
+      const value = +counter.getAttribute('akhi');
+      const data = +counter.innerText;
+     
+      const time = value / speed;
+     if(data < value) {
+          counter.innerText = Math.ceil(data + time);
+          setTimeout(animate, 1);
+        }else{
+          counter.innerText = value;
+        }
+     
+   }
+   
+   animate();
+});
+
+const myNav = document.getElementById('header')
+
+window.onscroll = function() {
+  if(window.scrollY > window.innerHeight){
+    myNav.classList.add('scrolled')
+  }else{
+    myNav.classList.remove('scrolled')
+  }
+}
